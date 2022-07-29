10fdb572e038c85375ac84307a5c0452aff9156e
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-07-29 21:19:26 +0530

    outputting added

---
+++ 01_basics_of_java/_02_Outputting.java
@@ -0,0 +1,10 @@
+class _02_Outputting {
+    public static void main(String[] args) {
+        // if we use println the next statement will come in the new line
+        System.out.println("First way is using println method");
+
+        // if we use print the next statement will come in the same line
+        System.out.print("Second way is ");
+        System.out.print("print method");
+    }
+}
