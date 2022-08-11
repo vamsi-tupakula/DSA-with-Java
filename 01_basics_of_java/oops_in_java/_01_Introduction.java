528ee7eec506d343a9f2bd5e9eaf67eeff7dd01d
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-11 21:08:55 +0530

    greet function,constructor without parameter added

--- 01_basics_of_java/oops_in_java/_01_Introduction.java
+++ 01_basics_of_java/oops_in_java/_01_Introduction.java
@@ -2,11 +2,15 @@ package oops_in_java;
 
 public class _01_Introduction {
     public static void main(String[] args) {
-        Student student1 = new Student("Robert",2309);
+        Student student1 = new Student();
+        Student student2 = new Student("Robert",2309);
         // student1.name = "Java";
         // student1.roll_no = 1201;
         System.out.println(student1.name);
+        System.out.println(student2.name);
         System.out.println(student1.roll_no);
+        System.out.println(student2.roll_no);
+        student1.greet();
     }
 }
 
@@ -14,8 +18,17 @@ class Student {
     String name;
     int roll_no;
 
+    Student() {
+        this.name = "guest-user";
+        this.roll_no = 1234;
+    }
+
     Student(String name,int roll_no) {
         this.name = name;
         this.roll_no = roll_no;
+    }
+
+    void greet() {
+        System.out.println("Hello, My name is " + this.name);
     }
 }
