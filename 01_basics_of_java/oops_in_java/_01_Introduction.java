eb04b42bb4e2ab0eca346245db787f27add602ef
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-11 21:00:00 +0530

    constructor added

--- 01_basics_of_java/oops_in_java/_01_Introduction.java
+++ 01_basics_of_java/oops_in_java/_01_Introduction.java
@@ -2,9 +2,9 @@ package oops_in_java;
 
 public class _01_Introduction {
     public static void main(String[] args) {
-        Student student1 = new Student();
-        student1.name = "Java";
-        student1.roll_no = 1201;
+        Student student1 = new Student("Robert",2309);
+        // student1.name = "Java";
+        // student1.roll_no = 1201;
         System.out.println(student1.name);
         System.out.println(student1.roll_no);
     }
@@ -13,4 +13,9 @@ public class _01_Introduction {
 class Student {
     String name;
     int roll_no;
+
+    Student(String name,int roll_no) {
+        this.name = name;
+        this.roll_no = roll_no;
+    }
 }
