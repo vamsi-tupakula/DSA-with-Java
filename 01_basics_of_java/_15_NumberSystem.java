ecc837f72facedeb4ee61d9cc1874fb50cad05d0
Vamsi <vamsitupakula474@gmail.com>
2022-10-06 11:11:40 +0530

    binary to decimal convertor...

--- 01_basics_of_java/_15_NumberSystem.java
+++ 01_basics_of_java/_15_NumberSystem.java
@@ -2,6 +2,8 @@ public class _15_NumberSystem {
     public static void main(String[] args) {
         int decimal = 10;
         toBinary(decimal);
+        String str = "101101";
+        System.out.println(toDecimal(str));
     }
     private static void toBinary(int decimal){
         int[] ans = new int[40];
@@ -14,5 +16,15 @@ public class _15_NumberSystem {
             System.out.print(ans[i]);
         }
         System.out.println(); // new line
+    }
+    private static int toDecimal(String str) {
+        int ans = 0;
+        int bit_value = 1;
+        for(int i = str.length() - 1;i >= 0;i--) {
+            int bit = Character.getNumericValue(str.charAt(i));
+            ans += bit*bit_value;
+            bit_value *= 2;
+        }
+        return ans;
     }
 }
