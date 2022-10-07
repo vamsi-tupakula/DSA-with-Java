65ebd0d0ebf38215e76845afce98f6b5ab053972
Vamsi <vamsitupakula474@gmail.com>
2022-10-07 21:42:15 +0530

    prime_or_not modified...

--- 01_basics_of_java/_07_PrimeOrNot.java
+++ 01_basics_of_java/_07_PrimeOrNot.java
@@ -2,14 +2,6 @@ import java.util.Scanner;
 
 public class _07_PrimeOrNot {
     public static void main(String[] args) {
-        /**
-         * Note 1 : Time complexity : O(n)
-         * If a number is a prime it must be divisible by 1 and itself only.
-         * Note 2 : Time complexity : O(sqrt(n))
-         * Every prime number can be expressed in the form of 6n+1 or 6n-1 except 2 and 3
-         * Note 3 : Time complexity : O(1)
-         * If a number is not a prime then it must be divisible by aleast one number between 1 and sqrt(num)
-         */
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter any number :: ");
         int num = sc.nextInt();
