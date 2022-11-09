5f32a1017d817969bc23f6bea7866f1f26e1c3ce
Vamsi <vamsitupakula474@gmail.com>
2022-11-09 09:45:26 +0530

    rectangle, triangle patterns added --basic

---
+++ 01_basics_of_java/_17_Patterns.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class _17_Patterns {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Rows : ");
+        int m = sc.nextInt();
+        System.out.print("Columns : ");
+        int n = sc.nextInt();
+        patterns(m,n);
+        sc.close();
+    }
+
+    private static void patterns(int m, int n) {
+        // rectangular pattern
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        // triangule pattern
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<=i;j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        // reverse triangle
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<m-i;j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        // number triangle
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<=i;j++) {
+                System.out.print(j+1 + " ");
+            }
+            System.out.println();
+        }
+    }
+}
